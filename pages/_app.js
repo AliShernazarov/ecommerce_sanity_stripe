65bfc5edf73497679991078145a7018cf49f2878
MAC <mac@MacBook-Air-ALI.local>
2023-06-09 16:41:17 +0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+import React from "react";
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import Layout from "../components/Layout";
+import { StateContext } from "../context/StateContext";
+import { Toaster } from "react-hot-toast";
+
+function MyApp({ Component, pageProps }) {
+	return (
+		<StateContext>
+			<Layout>
+				<Toaster />
+				<Component {...pageProps} />
+			</Layout>
+		</StateContext>
+	);
 }
+
+export default MyApp;
---
+++ sanity_ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'hvzch2d0',
+    dataset: 'production'
+  }
+})
